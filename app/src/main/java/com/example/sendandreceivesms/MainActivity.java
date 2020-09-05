2c8ddf470107189cdcf11a58bd4a0f6219f9be8e
landry333 <57639988+Landry333@users.noreply.github.com>
2020-09-05 16:54:27 -0400

    adding sms sending on mainactivity

--- app/src/main/java/com/example/sendandreceivesms/MainActivity.java
+++ app/src/main/java/com/example/sendandreceivesms/MainActivity.java
@@ -11,9 +11,26 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextNumber;
+    private EditText editTextMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+
+        editTextMessage = findViewById(R.id.editText);
+        editTextNumber = findViewById(R.id.editTextNumber);
+    }
+
+    public void sendSMS(View view){
+
+        String message = editTextMessage.getText().toString();
+        String number = editTextNumber.getText().toString();
+
+        SmsManager mySmsManager = SmsManager.getDefault();
+        mySmsManager.sendTextMessage(number,null, message, null, null);
     }
 }
+
