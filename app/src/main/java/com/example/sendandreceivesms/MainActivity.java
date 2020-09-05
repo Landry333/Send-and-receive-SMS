2f4694e0b7418eb999bbb0f9e0ce2945126afb40
Landry <landrysigni@yahoo.com>
2020-09-05 16:48:42 -0400

    adding imports

--- app/src/main/java/com/example/sendandreceivesms/MainActivity.java
+++ app/src/main/java/com/example/sendandreceivesms/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sendandreceivesms;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
